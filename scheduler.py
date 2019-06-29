77d444b425de4e62d8e868911f71c6308573853f
parsam97 <parsam97@users.noreply.github.com>
2019-06-29 12:46:52 +0200

    Main file

---
+++ scheduler.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import csv, pprint
+
+class Offer:
+    def __init__(self):
+        self.requirements = {}
+        self.possibilities = []
+        self.impossibilities = {}
+        self.makeOffer()
+
+    def checkPossibilities(self):
+        for index, row in dfRecipes.iterrows():
+            self.impossibilities[row['name']] = []
+            for requirement in row['list_of_ingredients'].split(','):
+                ingredient = requirement.split(':')[0]
+                requiredAmount = requirement.split(':')[1]
+                self.requirements[ingredient] = requiredAmount
+                if float(inventory.amountOf(ingredient)) < float(requiredAmount):
+                    self.impossibilities[row['name']].append("Missing {0}".format(ingredient))
+                else:
+                    pass
+            if len(self.impossibilities[row['name']]) == 0:
+                self.possibilities.append(row['name'])
+                del self.impossibilities[row['name']]
+            else:
+                pass
+
+        return self.possibilities
+
+    def makeOffer(self):
+        possibilities = self.checkPossibilities()
+
+
+class Ingredient:
+    def __init__(self, name, weight, quantity):
+        self.name = name
+        self.weight = weight
+        self.quantity = quantity
+    def getName(self):
+        return self.name
+    def getWeight(self):
+        return self.weight
+    def getQuantity(self):
+        return self.quantity
+
+class Inventory():
+    def __init__(self):
+        self.inventory = []
+        self.buildInventory()
+
+    def buildInventory(self):
+        for index, row in dfInventory.iterrows():
+            self.inventory.append(Ingredient(row['name'],row['weight'],row['quantity']))
+
+    def getInventoryList(self):
+        return self.inventory
+
+    def getIngredient(self, name):
+        for ingredient in self.inventory:
+            if ingredient.getName() == name:
+                return ingredient
+            else:
+                pass
+
+    def amountOf(self, name):
+        currIngredient = self.getIngredient(name)
+        if currIngredient.getWeight() == '':
+            return currIngredient.getQuantity()
+        else:
+            return currIngredient.getWeight()
+
+if __name__ == "__main__":
+    dataAddress = 'Budget.xlsx'
+    dfIngredients = pd.read_excel(dataAddress,'Ingredients')
+    dfRecipes = pd.read_excel(dataAddress,'Recipes')
+    dfInventory = pd.read_excel(dataAddress,'Inventory')
+    dfInventory.replace(pd.np.nan, '', regex=True, inplace=True)
+    inventory = Inventory()
+
+    a = Offer()
