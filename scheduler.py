cbbc419fc644521e228df52269179135a7f92eb1
parsam97 <parsam97@users.noreply.github.com>
2019-06-30 16:26:04 +0200

    major changes... can't stop

--- scheduler.py
+++ scheduler.py
@@ -1,34 +1,49 @@
 import pandas as pd
-import csv, pprint
+import numpy as np
+from pandas.io.json import json_normalize
+from collections import OrderedDict
+import csv, pprint, json
 
 class Inventory():
     """An object holding all inventory elements within a Pandas DataFrame"""
     def __init__(self, inventory):
-        self.inventory = inventory
+        self.inventory = pd.DataFrame(inventory)
 
     def getDf(self):
         """Returns self.inventory DataFrame"""
         return self.inventory
 
+    def selectFiltered(self, column, params):
+        """"""
+
+    def changeInWeight(self, df, amt):
+        """Takes in a DataFrame (df) and adds the amount (amt) to the 'Weight in store' value of that ingredient"""
+        mask = (self.inventory['Name'] not in list(df['Name']))
+        self.inventory['Weight in store'] = self.inventory['Name'].apply(lambda x: x+amt if x in list(df['Name']) else x)
+
+
 class RecipeBook():
     """An object holding all recipe elements within a Pandas DataFrame"""
     def __init__(self, recipes):
-        self.recipes = recipes
+        self.recipes = pd.DataFrame(recipes)
 
     def getDf(self):
         """Returns self.inventory DataFrame"""
         return self.recipes
 
-class 
-
 def main():
     dataAddress = 'Budget.xlsx'
 
-    inventory = Inventory(pd.read_excel(dataAddress,'Inventory'))
-    recipeBook = RecipeBook(pd.read_excel(dataAddress,'Recipes'))
+    with open('inventory.json') as f:
+        inventory = Inventory(json.load(f, object_pairs_hook=OrderedDict))
 
-    print(inventory.getDf())
-    print(recipeBook.getDf())
+    with open('recipes.json') as f:
+        recipeBook = RecipeBook(json.load(f, object_pairs_hook=OrderedDict))
+
+    inventory.changeInWeight(inventory.getDf(), 1500)
+
+    print(inventory.getDf().head())
+    print(recipeBook.getDf().head())
 
 if __name__ == "__main__":
     main()
